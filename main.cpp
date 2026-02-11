f5d1e92895fd494cdc2f31d5ce9af413b1d2e54c
Loma <lomakim@gmail.com>
2026-02-10 19:42:04 -0800

    update

--- main.cpp
+++ main.cpp
@@ -1,22 +1,29 @@
+// COMSC-210 | Lab 5 | Loma Kim
+/* In this lab I have taken the starter code provided by Professor Dietrich 
+   and updated the code to use pointers instead of the int variables*/
 #include <iostream>
 using namespace std;
 
-int swap(int &, int &);
+int swap(int *, int *);
 
 int main() {
+    int *xPtr = nullptr, *yPtr = nullptr;
     int x = 5, y = 10;
-    cout << "x = " << x << "  | y = " << y << endl;
+    xPtr = &x;
+    yPtr = &y;
+
+    cout << "x = " << *xPtr << "  | y = " << *yPtr << endl;
 
     cout << "Swapping...\n";
-    int sum = swap(x, y);
-    cout << "x = " << x << " | y = " << y << endl;
+    int sum = swap(xPtr, yPtr);
+    cout << "x = " << *xPtr << " | y = " << *yPtr << endl;
     cout << "sum = " << sum << endl;
     return 0;
 }
 
-int swap(int &a, int &b) {
-    int temp = a;
-    a = b;
-    b = temp;
-    return a + b;
+int swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+    return *a + *b;
 }
